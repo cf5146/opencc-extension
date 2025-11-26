3744921fd06c18a4388330f3f8fa3904872f2067
cf5146 <cf5146@hotmail.com>
2025-11-26 15:47:23 -0600

    chore: remove legacy JavaScript source files

--- src/conversion.js
+++
@@ -1,9 +0,0 @@
-export {
-  convertAllNewTextNodes,
-  convertPlainText,
-  convertSelection,
-  convertTextNode,
-  convertTitle,
-  hasConverted,
-  resetConversionCache,
-} from "./core/conversion.js";
